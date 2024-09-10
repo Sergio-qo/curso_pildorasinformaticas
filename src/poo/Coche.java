5cb7e291c79929bcaa3f328f6432317b5463ab3a
23889767N <23889767N@SGAI-M-050.orgc.csic.es>
2024-09-10 13:26:14 +0200

    Upgrades and refactor

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -20,7 +20,7 @@ public class Coche {
 	}
 	
 	public String dime_datos_generales() {
-		return "La plataforma del vehículo tiene " + ruedas + " ruedas. " + "Mide " +largo/1000 + "metros con un ancho de " + ancho + "cm y un peso de plataforma de " + peso_plataforma + " kg";
+		return "La plataforma del vehículo tiene " + ruedas + " ruedas. " + "Mide " +largo/1000 + " metros con un ancho de " + ancho + "cm y un peso de plataforma de " + peso_plataforma + " kg";
 	}
 	
 	public void establece_color(String color_coche) {
@@ -32,7 +32,7 @@ public class Coche {
 	}
 	
 	public void configura_asientos(String asientos_cuero) {
-		if(asientos_cuero.toLowerCase() == "si") {
+		if(asientos_cuero.equalsIgnoreCase("si")) {
 			this.asientos_cuero = true;		
 		}else {
 			this.asientos_cuero = false;
@@ -46,6 +46,46 @@ public class Coche {
 		else {
 			return "El coche tiene asientos de serie";
 		}
+	}
+	
+	public void configura_climatizador(String climatizador) {
+		if(climatizador.equalsIgnoreCase("si")) {
+			this.climatizador = true;
+		}else {
+			this.climatizador = false;
+		}
+	}
+	
+	public String dime_climatizador(){
+		if (climatizador) {
+			return "El coche incorpora climatizdor";
+		}else {
+			return "El coche no tiene climatizador";
+		}
+	}
+	
+	public String dime_peso_coche() {
+		int peso_carroceria=500;
+		peso_total=peso_plataforma+peso_carroceria;
+		if(asientos_cuero) {
+			peso_total=peso_total+50;
+		}
+		if(climatizador) {
+			peso_total=peso_total+20;
+		}
+		return "El peso del coche es " + peso_total;
+	}
+	
+	public int precio_coche() {
+		int precio_final = 10000;
+		
+		if(asientos_cuero) {
+			precio_final+=2000;
+		}
+		if(climatizador) {
+			precio_final+=1500;
+		}
+		return precio_final;
 	}
 	
 }
--- src/poo/UsoCoche.java
+++ src/poo/UsoCoche.java
@@ -1,14 +1,20 @@
 package poo;
 
+import javax.swing.JOptionPane;
+
 public class UsoCoche {
 
 	public static void main(String[] args) {
-		Coche renault = new Coche();
-		renault.configura_asientos("si");
-		System.out.println(renault.dime_datos_generales());
-		renault.establece_color("amarillo");
-		System.out.println(renault.dime_color());
-		System.out.println(renault.dime_asientos());
+		Coche micoche = new Coche();
+		micoche.configura_asientos(JOptionPane.showInputDialog("Introduce asientos"));
+		System.out.println(micoche.dime_datos_generales());
+		micoche.establece_color(JOptionPane.showInputDialog("Introduce color"));
+		System.out.println(micoche.dime_color());
+		System.out.println(micoche.dime_asientos());
+		micoche.configura_climatizador(JOptionPane.showInputDialog("Introduce climatizador"));
+		System.out.println(micoche.dime_climatizador());
+		System.out.println(micoche.dime_peso_coche());
+		System.out.println("El precio final del cohce es: " + micoche.precio_coche());
 	}
 
 }
