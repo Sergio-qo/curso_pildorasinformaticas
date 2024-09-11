d052ffa5158bb9f8931b503493b91fa0c01173a1
23889767N <23889767N@sgai-m-050.sgai.csic.es>
2024-09-11 11:36:02 +0200

    Añadir clase empleado

---
+++ src/poo/Uso_Empleado.java
@@ -0,0 +1,25 @@
+package poo;
+
+import java.util.Date;
+
+public class Uso_Empleado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+class Empleado{
+	
+	public Empleado(String nom, double sue, int anio, int mes, int dia) {
+		
+	}
+	
+	private String nombre;
+	private double sue;
+	private int anio;
+	private Date altaContrato;
+}
